b199543caf0697d1e97d0fa3ad4513b6ae925f66
AnAnnoyingCat home-desktop <cwernke@student.ethz.ch>
2025-10-24 18:45:40 +0200

    it works

--- main.cpp
+++ main.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
 #include <cxxopts.hpp>
 #include <Magick++.h>
+#include <fstream>
 
 using namespace Magick; 
 
 // ASCII Parameters
 std::string in_filepath;
 std::string out_filepath;
-int target_width;
-int target_height;
-int line_space;
+double target_width = 0;
+double target_height = 0;
+double squishfactor = 1.0;
 bool inverted = false;
+double pixelRatio = 2.6666666666;
 
 // Declare functions
 std::string convertImage();
@@ -44,14 +46,17 @@ constexpr auto LUT_BOW = makeLUT(true);   // black on white
 constexpr auto LUT_WOB = makeLUT(false);  // white on black
 
 int main(int argc, char const *argv[]) {
+	// Set up Magick
+	InitializeMagick(*argv);
+
 	// =============== Read Options and set up render =============== //
 	cxxopts::Options options("Ascii renderer", "Renders given image in ASCII");
 	options.add_options()
 		("i,input", "Path to the input file (required)", cxxopts::value<std::string>())
 		("o,output", "Path to the output .txt file", cxxopts::value<std::string>()->default_value("out.txt"))
-		("w,width", "Target ASCII art character width", cxxopts::value<int>()->default_value("400"))
-		("t,height", "Target ASCII art character height. If both height and width are given, width is prioritized", cxxopts::value<int>()->default_value("200"))
-		("s,space", "Space between the lines of the render. Adjust this if your image is squished or stretched vertically. Larger value -> more squished.", cxxopts::value<int>()->default_value("7"))
+		("w,width", "Target ASCII art character width", cxxopts::value<int>())
+		("t,height", "Target ASCII art character height. If both height and width are given, width is prioritized", cxxopts::value<int>())
+		("s,squishfactor", "Adjust this if your image is squished or stretched vertically. Larger value -> more squished.", cxxopts::value<double>()->default_value("1.0"))
 		("n,invert", "Inverts the colors of the ascii art, from white on black to black on white")
 		("h,help", "Show this help page");
 
@@ -67,9 +72,10 @@ int main(int argc, char const *argv[]) {
 			throw std::invalid_argument("Did not recieve input path.");
 		}
 		out_filepath = result["output"].as<std::string>();
-		target_width = result["width"].as<int>();
-		target_height = result["height"].as<int>();
-		line_space = result["space"].as<int>();
+		if (result.count("width")) target_width = result["width"].as<int>();
+		if (result.count("height")) target_height = result["height"].as<int>();
+		squishfactor = result["squishfactor"].as<double>();
+		pixelRatio *= squishfactor;
 		if (result.count("invert")) {
 			inverted = true;
 		}
@@ -78,28 +84,84 @@ int main(int argc, char const *argv[]) {
 		return 1;
 	}
 	
+	// get the image
 	std::string art = convertImage();
 
+	// store it
+	std::ofstream out(out_filepath);
+	if (!out) {
+		throw std::runtime_error("Could not open output file: " + out_filepath);
+	}
+	out << art;
+	out.close();
 	return 0;
 }
 
-
+/**
+ * @brief Reads the input image. Then applies a light blur to denoise it and converts it to ascii characters
+ * 
+ *
+ */
 std::string convertImage() {
 	std::array<std::string_view, 256> lut;
+	std::string art = "";
+	
 	if (!inverted) {
 		lut = LUT_WOB;
 	} else {
 		lut = LUT_BOW;
 	}
 	try {
+		// Get the image
 		Image input(in_filepath);
+
+		int width = input.columns();
+		int height = input.rows();
+		art.reserve(width * (height + 1));
+
+		// Blur it to denoise it
 		input.gaussianBlur(0,1.0);
-		input.write("output.png");
+		input.write("step1blurred.png");
+
+		// Calculate target size keeping aspect ratio in mind
+		if (target_width != 0) {
+			target_height = height * (target_width / width) / pixelRatio;
+		} else {
+			if (target_height == 0) {
+				target_width = 400;
+				target_height = height * (target_width / width) / pixelRatio;
+			} else {
+				target_width = (width * target_height * pixelRatio) / height;
+			}
+		}
+
+		input.resize(Geometry(std::to_string(std::lround(target_width)) + "x" + std::to_string(std::lround(target_height)) + "!"));
+		input.write("step2resized.png");
+		
+		width = input.columns();
+		height = input.rows();
+
+		input.type(GrayscaleType);
+		input.write("step3grayscale.png");
+
+		// Create view of the pixel cache
+		Pixels view(input);
+
+		// Get a pointer to the pixel data
+		const PixelPacket* pixels = view.getConst(0, 0, input.columns(), input.rows());
+
+		const double scale = 255.0 / QuantumRange;
+		for (int j = 0; j < height; j++) {
+			for (int i = 0; i < width; i++) {
+				const int brightness = static_cast<int>(pixels[j * width + i].red * scale);
+				art += lut[brightness];
+			}
+			art += '\n';
+		}
+
 	} catch (const std::exception& e) {
 		std::cerr << "Error reading / editing image: " << e.what() << std::endl;
 	}
 	
-
-
-	return "";
+	return art;
 }
